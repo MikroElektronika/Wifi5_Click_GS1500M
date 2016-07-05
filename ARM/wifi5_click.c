c5a2af580cb0e8ac5df3333ed3ed326143ce9193
ViktorMilovanovic <viktor.milovanovic@mikroe.com>
2016-07-05 11:06:30 +0200

    Minor changes

--- ARM/wifi5_click.c
+++ ARM/wifi5_click.c
@@ -21,197 +21,197 @@ bool first_flag;
 
 void clear_serial_buffer()
 {
-  uint32_t i;
+	uint32_t i;
 
-  NVIC_IntDisable( IVT_INT_USART3 );
+	NVIC_IntDisable( IVT_INT_USART3 );
 
-  for (i=0; i<1000; i++)
-      serial_buffer[i] = 0;
+	for (i = 0; i < 1000; i++)
+		serial_buffer[i] = 0;
 
-  bfr_ctr = 0;
+	bfr_ctr = 0;
 
-  NVIC_IntEnable( IVT_INT_USART3 );
+	NVIC_IntEnable( IVT_INT_USART3 );
 }
 
 void flags_false ()
 {
-    response_finished = false;
-    ok_response = false;
-    first_flag = false;
+	response_finished = false;
+	ok_response = false;
+	first_flag = false;
 }
 
 void display_init()
 {
-    TFT_Init_ILI9341_8bit( 320, 240 );
-    TFT_BLED = 1;
-    TFT_Set_Pen( CL_WHITE, 1 );
-    TFT_Set_Brush( 1,CL_WHITE,0,0,0,0 );
-    TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
-    TFT_Fill_Screen( CL_WHITE );
-    TFT_Set_Pen( CL_BLACK, 1 );
-    TFT_Line( 20,  40, 300,  40 );
-    TFT_Line( 20, 220, 300, 220 );
-    TFT_Set_Font( &HandelGothic_BT21x22_Regular , CL_RED, FO_HORIZONTAL );
-    TFT_Write_Text( "WiFi 5 click", 95, 14 );
-    TFT_Set_Font( &Verdana12x13_Regular, CL_BLACK, FO_HORIZONTAL );
-    TFT_Write_Text("EasyMx PRO v7 for STM32", 19, 223);
-    TFT_Set_Font( &Verdana12x13_Regular, CL_RED, FO_HORIZONTAL );
-    TFT_Write_Text( "www.mikroe.com", 200, 223 );
-    TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
-    Delay_ms(1000);
+	TFT_Init_ILI9341_8bit( 320, 240 );
+	TFT_BLED = 1;
+	TFT_Set_Pen( CL_WHITE, 1 );
+	TFT_Set_Brush( 1, CL_WHITE, 0, 0, 0, 0 );
+	TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
+	TFT_Fill_Screen( CL_WHITE );
+	TFT_Set_Pen( CL_BLACK, 1 );
+	TFT_Line( 20,  40, 300,  40 );
+	TFT_Line( 20, 220, 300, 220 );
+	TFT_Set_Font( &HandelGothic_BT21x22_Regular , CL_RED, FO_HORIZONTAL );
+	TFT_Write_Text( "WiFi 5 click", 95, 14 );
+	TFT_Set_Font( &Verdana12x13_Regular, CL_BLACK, FO_HORIZONTAL );
+	TFT_Write_Text("EasyMx PRO v7 for STM32", 19, 223);
+	TFT_Set_Font( &Verdana12x13_Regular, CL_RED, FO_HORIZONTAL );
+	TFT_Write_Text( "www.mikroe.com", 200, 223 );
+	TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
+	Delay_ms(1000);
 }
 
 void parse_data ()
 {
-   static volatile char *xml_ptr;
-   char city_str[9];
-   char temperature_string[12];
-   char humidity_string[8];
-   char pressure_string[10];
-   int temp;
+	static volatile char *xml_ptr;
+	char city_str[9];
+	char temperature_string[12];
+	char humidity_string[8];
+	char pressure_string[10];
+	int temp;
 
 
-   xml_ptr = strstr(serial_buffer, "name");
+	xml_ptr = strstr(serial_buffer, "name");
 
-   strncpy(city_str, xml_ptr+6, 8);
-   city_str[8]='\0';
+	strncpy(city_str, xml_ptr + 6, 8);
+	city_str[8] = '\0';
 
-   TFT_Write_Text("City: ",10,50);
-   TFT_Write_Text(city_str, 50,50);
+	TFT_Write_Text("City: ", 10, 50);
+	TFT_Write_Text(city_str, 50, 50);
 
-   xml_ptr = strstr(serial_buffer, "temperature value");
-   strncpy(temperature_string, xml_ptr+19, 5);
-   temperature_string[5] = '\0';
+	xml_ptr = strstr(serial_buffer, "temperature value");
+	strncpy(temperature_string, xml_ptr + 19, 5);
+	temperature_string[5] = '\0';
 
-   temp = atoi(temperature_string);
-   temp = temp - 273;
-   inttostr(temp, temperature_string);
-   TFT_Write_Text("Temperature: ", 10,80);
-   TFT_Write_Text(temperature_string, 100,80);
-   TFT_Write_Text("Celsius", 200,80);
+	temp = atoi(temperature_string);
+	temp = temp - 273;
+	inttostr(temp, temperature_string);
+	TFT_Write_Text("Temperature: ", 10, 80);
+	TFT_Write_Text(temperature_string, 100, 80);
+	TFT_Write_Text("Celsius", 200, 80);
 
-   xml_ptr = strstr(serial_buffer,"humidity value=");
-   strncpy(humidity_string, xml_ptr+16, 2);
-   humidity_string[2] = '\0';
-   TFT_Write_Text("Humidity: ", 10,110);
-   TFT_Write_Text(humidity_string, 110,110);
-   TFT_Write_Text("%", 200,110);
+	xml_ptr = strstr(serial_buffer, "humidity value=");
+	strncpy(humidity_string, xml_ptr + 16, 2);
+	humidity_string[2] = '\0';
+	TFT_Write_Text("Humidity: ", 10, 110);
+	TFT_Write_Text(humidity_string, 110, 110);
+	TFT_Write_Text("%", 200, 110);
 
-   xml_ptr = strstr(serial_buffer,"pressure value=");
-   strncpy(pressure_string, xml_ptr+16, 4);
-   pressure_string [4] = '\0';
-   TFT_Write_Text("Pressure: ", 10,140);
-   TFT_Write_Text(pressure_string, 110,140);
-   TFT_Write_Text("hPa ", 200,140);
+	xml_ptr = strstr(serial_buffer, "pressure value=");
+	strncpy(pressure_string, xml_ptr + 16, 4);
+	pressure_string [4] = '\0';
+	TFT_Write_Text("Pressure: ", 10, 140);
+	TFT_Write_Text(pressure_string, 110, 140);
+	TFT_Write_Text("hPa ", 200, 140);
 
 }
 
 
 void main()
 {
-     // initialize the UART for monitoring the program
-     UART1_Init_Advanced( 9600,  _UART_8_BIT_DATA,
-                                  _UART_NOPARITY,
-                                 _UART_ONE_STOPBIT,
-                                 &_GPIO_MODULE_USART1_PA9_10 );
-     Delay_ms(300);
-     UART1_Write_Text("Uart initialized\r\n");
+	// initialize the UART for monitoring the program
+	UART1_Init_Advanced( 9600,  _UART_8_BIT_DATA,
+	                     _UART_NOPARITY,
+	                     _UART_ONE_STOPBIT,
+	                     &_GPIO_MODULE_USART1_PA9_10 );
+	Delay_ms(300);
+	UART1_Write_Text("Uart initialized\r\n");
 
-     Delay_ms(5000);
-     // initialize the UART for communication between the MCU and the WiFi 5 Click
-     UART3_Init_Advanced( 9600, _UART_8_BIT_DATA,
-                                _UART_NOPARITY,
-                                _UART_ONE_STOPBIT,
-                                &_GPIO_MODULE_USART3_PD89);
+	Delay_ms(5000);
+	// initialize the UART for communication between the MCU and the WiFi 5 Click
+	UART3_Init_Advanced( 9600, _UART_8_BIT_DATA,
+	                     _UART_NOPARITY,
+	                     _UART_ONE_STOPBIT,
+	                     &_GPIO_MODULE_USART3_PD89);
 
-     Delay_ms(5000);
-
-
-     UART1_Write_Text("Uarts ready\r\n");
-
-     Delay_ms(1000);
-     
-     // set up the UART interrupt
-     RXNEIE_USART3_CR1_bit = 1;
-     NVIC_IntEnable( IVT_INT_USART3 );
-     EnableInterrupts();
-
-     // clear the buffer and the flags, setting them ready for the program to start
-     clear_serial_buffer();
-     flags_false();
-     UART3_WRITE_TEXT("AT+WPAPSK=MikroE Public,mikroe.guest\r\n");   // compute the PSK from SSID and PassPhrase
-     while(response_finished == false);                              // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                                // print out the response
-
-     clear_serial_buffer();                                          // clear the buffer and flags
-     flags_false();
-     Delay_ms(300);
-     UART3_WRITE_TEXT("AT+WA=MikroE Public\r\n");                    // try to connect to MikroE Public network
-     while (response_finished == false);                             // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                                // print out the response
-
-     clear_serial_buffer();                                          // clear the buffer and flags
-     flags_false();
-     UART3_WRITE_TEXT("at+httpopen=api.openweathermap.org\r\n");     // open the api.openweathermap.org web page
-     while (response_finished == false);                             // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                                // print out the response
-
-     clear_serial_buffer();                                          // clear the buffer and flags
-     flags_false();
-     UART3_WRITE_TEXT("at+httpconf=11,api.openweathermap.org\r\n");  // configure the right http parameters
-     while (response_finished == false);                             // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                                // print out the response
-
-     clear_serial_buffer();                                          // clear the buffer and flags
-     flags_false();
-     UART3_WRITE_TEXT("at+httpsend=0,1,5,/data/2.5/weather?q=Belgrade&mode=xml&appid=438f494400615270b4d2c1f9d563bf19\r\n");   // send the http request
+	Delay_ms(5000);
 
 
-     while ( bfr_ctr <  834 );                                       // wait until the buffer fills up with data
-     UART1_write_text("Data fetched \r\n\r\n");                      // notify the user that we have fetched the data from the web
+	UART1_Write_Text("Uarts ready\r\n");
+
+	Delay_ms(1000);
+
+	// set up the UART interrupt
+	RXNEIE_USART3_CR1_bit = 1;
+	NVIC_IntEnable( IVT_INT_USART3 );
+	EnableInterrupts();
+
+	// clear the buffer and the flags, setting them ready for the program to start
+	clear_serial_buffer();
+	flags_false();
+	UART3_WRITE_TEXT("AT+WPAPSK=MikroE Public,mikroe.guest\r\n");   // compute the PSK from SSID and PassPhrase
+	while (response_finished == false);                             // wait for the "OK" response
+
+	UART1_WRITE_TEXT(serial_buffer);                                // print out the response
+
+	clear_serial_buffer();                                          // clear the buffer and flags
+	flags_false();
+	Delay_ms(300);
+	UART3_WRITE_TEXT("AT+WA=MikroE Public\r\n");                    // try to connect to MikroE Public network
+	while (response_finished == false);                             // wait for the "OK" response
+
+	UART1_WRITE_TEXT(serial_buffer);                                // print out the response
+
+	clear_serial_buffer();                                          // clear the buffer and flags
+	flags_false();
+	UART3_WRITE_TEXT("at+httpopen=api.openweathermap.org\r\n");     // open the api.openweathermap.org web page
+	while (response_finished == false);                             // wait for the "OK" response
+
+	UART1_WRITE_TEXT(serial_buffer);                                // print out the response
+
+	clear_serial_buffer();                                          // clear the buffer and flags
+	flags_false();
+	UART3_WRITE_TEXT("at+httpconf=11,api.openweathermap.org\r\n");  // configure the right http parameters
+	while (response_finished == false);                             // wait for the "OK" response
+
+	UART1_WRITE_TEXT(serial_buffer);                                // print out the response
+
+	clear_serial_buffer();                                          // clear the buffer and flags
+	flags_false();
+	UART3_WRITE_TEXT("at+httpsend=0,1,5,/data/2.5/weather?q=Belgrade&mode=xml&appid=438f494400615270b4d2c1f9d563bf19\r\n");   // send the http request
+
+
+	while ( bfr_ctr <  834 );                                       // wait until the buffer fills up with data
+	UART1_write_text("Data fetched \r\n\r\n");                      // notify the user that we have fetched the data from the web
 
 //********** PARSING PART***********************//
 
-       display_init();                                               // initialize the TFT display
-       parse_data();                                                 // parse the data and display it on the TFT
+	display_init();                                               // initialize the TFT display
+	parse_data();                                                 // parse the data and display it on the TFT
 
 }
 
 void wifi5_rx_isr( char rx_input )
 {
 
-        if (rx_input == 'K')
-        {
-            if (serial_buffer[bfr_ctr - 1] == 'O')
-               ok_response = true;
-        }
+	if (rx_input == 'K')
+	{
+		if (serial_buffer[bfr_ctr - 1] == 'O')
+			ok_response = true;
+	}
 
-        if (rx_input == '\n' && ok_response == true && serial_buffer[bfr_ctr - 1] == '\r')
-        {
-            if (first_flag != true) first_flag = true;
-            else if (rx_input == '\n' && first_flag == true && serial_buffer[bfr_ctr - 1] == '\r')
-                response_finished = true;
-        }
-        serial_buffer[bfr_ctr++] = rx_input;
+	if (rx_input == '\n' && ok_response == true && serial_buffer[bfr_ctr - 1] == '\r')
+	{
+		if (first_flag != true) first_flag = true;
+		else if (rx_input == '\n' && first_flag == true && serial_buffer[bfr_ctr - 1] == '\r')
+			response_finished = true;
+	}
+	serial_buffer[bfr_ctr++] = rx_input;
 
 }
 
 
 void WF_RX() iv IVT_INT_USART3 ics ICS_AUTO
 {
-     char tmp;
-     if( RXNE_USART3_SR_bit == 1)
-     {
-         if (bfr_ctr == 999)
-         {
-           UART1_WRITE_TEXT("CTR OVERFLOW");
-           while(1);
-         }
-       tmp = USART3_DR;
-       wifi5_rx_isr(tmp);
-     }
+	char tmp;
+	if ( RXNE_USART3_SR_bit == 1)
+	{
+		if (bfr_ctr == 999)
+		{
+			UART1_WRITE_TEXT("CTR OVERFLOW");
+			while (1);
+		}
+		tmp = USART3_DR;
+		wifi5_rx_isr(tmp);
+	}
 }
--- FT900/wifi5_click.c
+++ FT900/wifi5_click.c
@@ -65,8 +65,8 @@ void clear_serial_buffer()
 
   UART2_INT_ENABLE &= ~( 1 << RX_AVL_EN );
 
-  for (i=0; i<1000; i++)
-      serial_buffer[i] = 0;
+  for (i = 0; i < 1000; i++)
+    serial_buffer[i] = 0;
 
   bfr_ctr = 0;
 
@@ -75,177 +75,177 @@ void clear_serial_buffer()
 
 void flags_false ()
 {
-    response_finished = false;
-    ok_response = false;
-    first_flag = false;
+  response_finished = false;
+  ok_response = false;
+  first_flag = false;
 }
 
 void display_init()
 {
-    TFT_Init_ILI9341_8bit( 320, 240 );
-    TFT_BLED = 1;
-    TFT_Set_Pen( CL_AQUA, 1 );
-    TFT_Set_Brush( 1,CL_AQUA,0,0,0,0 );
-    TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
-    TFT_Fill_Screen( CL_AQUA );
-    TFT_Set_Pen( CL_BLACK, 1 );
-    TFT_Line( 20,  40, 300,  40 );
-    TFT_Line( 20, 220, 300, 220 );
-    TFT_Set_Font( &HandelGothic_BT21x22_Regular , CL_RED, FO_HORIZONTAL );
-    TFT_Write_Text( "WiFi 5 click", 95, 14 );
-    TFT_Set_Font( &Verdana12x13_Regular, CL_BLACK, FO_HORIZONTAL );
-    TFT_Write_Text("EasyMx PRO v7 for STM32", 19, 223);
-    TFT_Set_Font( &Verdana12x13_Regular, CL_RED, FO_HORIZONTAL );
-    TFT_Write_Text( "www.mikroe.com", 200, 223 );
-    TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
-    Delay_ms(1000);
+  TFT_Init_ILI9341_8bit( 320, 240 );
+  TFT_BLED = 1;
+  TFT_Set_Pen( CL_AQUA, 1 );
+  TFT_Set_Brush( 1, CL_AQUA, 0, 0, 0, 0 );
+  TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
+  TFT_Fill_Screen( CL_AQUA );
+  TFT_Set_Pen( CL_BLACK, 1 );
+  TFT_Line( 20,  40, 300,  40 );
+  TFT_Line( 20, 220, 300, 220 );
+  TFT_Set_Font( &HandelGothic_BT21x22_Regular , CL_RED, FO_HORIZONTAL );
+  TFT_Write_Text( "WiFi 5 click", 95, 14 );
+  TFT_Set_Font( &Verdana12x13_Regular, CL_BLACK, FO_HORIZONTAL );
+  TFT_Write_Text("EasyMx PRO v7 for STM32", 19, 223);
+  TFT_Set_Font( &Verdana12x13_Regular, CL_RED, FO_HORIZONTAL );
+  TFT_Write_Text( "www.mikroe.com", 200, 223 );
+  TFT_Set_Font( TFT_defaultFont, CL_BLACK, FO_HORIZONTAL );
+  Delay_ms(1000);
 }
 
 void parse_data ()
 {
-   char *xml_ptr;
-   char city_str[9];
-   char temperature_string[12];
-   char humidity_string[8];
-   char pressure_string[10];
-   int temp;
+  char *xml_ptr;
+  char city_str[9];
+  char temperature_string[12];
+  char humidity_string[8];
+  char pressure_string[10];
+  int temp;
 
 
-   xml_ptr = strstr(serial_buffer, "name");
+  xml_ptr = strstr(serial_buffer, "name");
 
-   strncpy(city_str, xml_ptr+6, 8);
-   city_str[8]='\0';
+  strncpy(city_str, xml_ptr + 6, 8);
+  city_str[8] = '\0';
 
-   TFT_Write_Text("City: ",10,50);
-   TFT_Write_Text(city_str, 50,50);
+  TFT_Write_Text("City: ", 10, 50);
+  TFT_Write_Text(city_str, 50, 50);
 
-   xml_ptr = strstr(serial_buffer, "temperature value");
-   strncpy(temperature_string, xml_ptr+19, 5);
-   temperature_string[5] = '\0';
+  xml_ptr = strstr(serial_buffer, "temperature value");
+  strncpy(temperature_string, xml_ptr + 19, 5);
+  temperature_string[5] = '\0';
 
-   temp = atoi(temperature_string);
-   temp = temp - 273;
-   inttostr(temp, temperature_string);
-   TFT_Write_Text("Temperature: ", 10,80);
-   TFT_Write_Text(temperature_string, 100,80);
-   TFT_Write_Text("Celsius", 200,80);
+  temp = atoi(temperature_string);
+  temp = temp - 273;
+  inttostr(temp, temperature_string);
+  TFT_Write_Text("Temperature: ", 10, 80);
+  TFT_Write_Text(temperature_string, 100, 80);
+  TFT_Write_Text("Celsius", 200, 80);
 
-   xml_ptr = strstr(serial_buffer,"humidity value=");
-   strncpy(humidity_string, xml_ptr+16, 2);
-   humidity_string[2] = '\0';
-   TFT_Write_Text("Humidity: ", 10,110);
-   TFT_Write_Text(humidity_string, 110,110);
-   TFT_Write_Text("%", 200,110);
+  xml_ptr = strstr(serial_buffer, "humidity value=");
+  strncpy(humidity_string, xml_ptr + 16, 2);
+  humidity_string[2] = '\0';
+  TFT_Write_Text("Humidity: ", 10, 110);
+  TFT_Write_Text(humidity_string, 110, 110);
+  TFT_Write_Text("%", 200, 110);
 
-   xml_ptr = strstr(serial_buffer,"pressure value=");
-   strncpy(pressure_string, xml_ptr+16, 4);
-   pressure_string [4] = '\0';
-   TFT_Write_Text("Pressure: ", 10,140);
-   TFT_Write_Text(pressure_string, 110,140);
-   TFT_Write_Text("hPa ", 200,140);
+  xml_ptr = strstr(serial_buffer, "pressure value=");
+  strncpy(pressure_string, xml_ptr + 16, 4);
+  pressure_string [4] = '\0';
+  TFT_Write_Text("Pressure: ", 10, 140);
+  TFT_Write_Text(pressure_string, 110, 140);
+  TFT_Write_Text("hPa ", 200, 140);
 }
 
 
 void main()
 {
-     char txt;
-     uint16_t debug_ctr;
-     uint16_t debug_tmp;
-     uint32_t print_ctr;
-     uint8_t ptr_ctr;
+  char txt;
+  uint16_t debug_ctr;
+  uint16_t debug_tmp;
+  uint32_t print_ctr;
+  uint8_t ptr_ctr;
 
-     // initialize the UART for monitoring the program
-     UART1_Init( 9600 );
-     Delay_ms(300);
-     UART1_Write_Text("Uart initialized\r\n");
+  // initialize the UART for monitoring the program
+  UART1_Init( 9600 );
+  Delay_ms(300);
+  UART1_Write_Text("Uart initialized\r\n");
 
-     Delay_ms(5000);
+  Delay_ms(5000);
 
-     // initialize the UART for communication between the MCU and the WiFi 5 Click
-     UART2_Init( 9600 );
-     Delay_ms(5000);
-
-
-     UART1_Write_Text("Uarts ready\r\n");
-     Delay_ms(1000);
-
-     // set up the UART interrupt
-     UART2_FCR |= ( 1 << FIFO_EN ) | ( 1 << RCVR_RST );
-     UART2_INT_ENABLE |= ( 1 << RX_AVL_EN );
-     IRQ_CTRL &= ~( 1 << GLOBAL_INTERRUPT_MASK );
-
-     // clear the buffer and the flags, setting them ready for the program to start
-     clear_serial_buffer();
-     flags_false();
-     UART2_WRITE_TEXT("AT+WPAPSK=MikroE Public,mikroe.guest\r\n");  // compute the PSK from SSID and PassPhrase
-     while(response_finished == false);                             // wait for the "OK" response
-     
-     UART1_WRITE_TEXT(serial_buffer);                               // print out the response
-
-     clear_serial_buffer();                                         // clear the buffer and flags
-     flags_false();
-     Delay_ms(300);
-     UART2_WRITE_TEXT("AT+WA=MikroE Public\r\n");                   // try to connect to MikroE Public network
-     while (response_finished == false);                            // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                               // print out the response
-
-     clear_serial_buffer();                                         // clear the buffer and flags
-     flags_false();
-     UART2_WRITE_TEXT("at+httpopen=api.openweathermap.org\r\n");    // open the api.openweathermap.org web page
-     while (response_finished == false);                            // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                               // print out the response
-
-     clear_serial_buffer();                                         // clear the buffer and flags
-     flags_false();
-     UART2_WRITE_TEXT("at+httpconf=11,api.openweathermap.org\r\n"); // configure the right http parameters
-     while (response_finished == false);                            // wait for the "OK" response
-
-     UART1_WRITE_TEXT(serial_buffer);                               // print out the response
-
-     clear_serial_buffer();                                         // clear the buffer and flags
-     flags_false();
-     UART2_WRITE_TEXT("at+httpsend=0,1,5,/data/2.5/weather?q=Belgrade&mode=xml&appid=438f494400615270b4d2c1f9d563bf19\r\n");  // send the http request
+  // initialize the UART for communication between the MCU and the WiFi 5 Click
+  UART2_Init( 9600 );
+  Delay_ms(5000);
 
 
-     while(bfr_ctr <  810 + 2 + 10 + 12);                           // wait until the buffer fills up with data
-     UART1_write_text("Data fetched \r\n\r\n");                     // notify the user that we have fetched the data from the web
+  UART1_Write_Text("Uarts ready\r\n");
+  Delay_ms(1000);
+
+  // set up the UART interrupt
+  UART2_FCR |= ( 1 << FIFO_EN ) | ( 1 << RCVR_RST );
+  UART2_INT_ENABLE |= ( 1 << RX_AVL_EN );
+  IRQ_CTRL &= ~( 1 << GLOBAL_INTERRUPT_MASK );
+
+  // clear the buffer and the flags, setting them ready for the program to start
+  clear_serial_buffer();
+  flags_false();
+  UART2_WRITE_TEXT("AT+WPAPSK=MikroE Public,mikroe.guest\r\n");  // compute the PSK from SSID and PassPhrase
+  while (response_finished == false);                            // wait for the "OK" response
+
+  UART1_WRITE_TEXT(serial_buffer);                               // print out the response
+
+  clear_serial_buffer();                                         // clear the buffer and flags
+  flags_false();
+  Delay_ms(300);
+  UART2_WRITE_TEXT("AT+WA=MikroE Public\r\n");                   // try to connect to MikroE Public network
+  while (response_finished == false);                            // wait for the "OK" response
+
+  UART1_WRITE_TEXT(serial_buffer);                               // print out the response
+
+  clear_serial_buffer();                                         // clear the buffer and flags
+  flags_false();
+  UART2_WRITE_TEXT("at+httpopen=api.openweathermap.org\r\n");    // open the api.openweathermap.org web page
+  while (response_finished == false);                            // wait for the "OK" response
+
+  UART1_WRITE_TEXT(serial_buffer);                               // print out the response
+
+  clear_serial_buffer();                                         // clear the buffer and flags
+  flags_false();
+  UART2_WRITE_TEXT("at+httpconf=11,api.openweathermap.org\r\n"); // configure the right http parameters
+  while (response_finished == false);                            // wait for the "OK" response
+
+  UART1_WRITE_TEXT(serial_buffer);                               // print out the response
+
+  clear_serial_buffer();                                         // clear the buffer and flags
+  flags_false();
+  UART2_WRITE_TEXT("at+httpsend=0,1,5,/data/2.5/weather?q=Belgrade&mode=xml&appid=438f494400615270b4d2c1f9d563bf19\r\n");  // send the http request
+
+
+  while (bfr_ctr <  810 + 2 + 10 + 12);                          // wait until the buffer fills up with data
+  UART1_write_text("Data fetched \r\n\r\n");                     // notify the user that we have fetched the data from the web
 
 //********** PARSING PART***********************//
 
-     display_init();                                                // initialize the TFT display
-     parse_data();                                                  // parse the data and display it on the TFT
+  display_init();                                                // initialize the TFT display
+  parse_data();                                                  // parse the data and display it on the TFT
 
 }
 
 void wifi5_rx_isr( char rx_input )
 {
 
-        if (rx_input == 'K')
-        {
-            if (serial_buffer[bfr_ctr - 1] == 'O')
-               ok_response = true;
-        }
+  if (rx_input == 'K')
+  {
+    if (serial_buffer[bfr_ctr - 1] == 'O')
+      ok_response = true;
+  }
 
-        if (rx_input == '\n' && ok_response == true && serial_buffer[bfr_ctr - 1] == '\r')
-        {
-            if (first_flag != true) first_flag = true;
-            else if (rx_input == '\n' && first_flag == true && serial_buffer[bfr_ctr - 1] == '\r')
-                response_finished = true;
-        }
-        serial_buffer[bfr_ctr++] = rx_input;
+  if (rx_input == '\n' && ok_response == true && serial_buffer[bfr_ctr - 1] == '\r')
+  {
+    if (first_flag != true) first_flag = true;
+    else if (rx_input == '\n' && first_flag == true && serial_buffer[bfr_ctr - 1] == '\r')
+      response_finished = true;
+  }
+  serial_buffer[bfr_ctr++] = rx_input;
 
 }
 
 void UART_RX_ISR() iv IVT_UART2_IRQ ics ICS_AUTO
 {
-     char tmp;
-     if (bfr_ctr == 999)
-     {  UART1_WRITE_TEXT("CTR OVERFLOW");
-        while(1);
-     }
-     tmp = UART2_Read();
-     wifi5_rx_isr(tmp);
+  char tmp;
+  if (bfr_ctr == 999)
+  { UART1_WRITE_TEXT("CTR OVERFLOW");
+    while (1);
+  }
+  tmp = UART2_Read();
+  wifi5_rx_isr(tmp);
 
 }
